63f1aad1403750a74aef3c33c13514b124b23dca
hayato-ui <hayato.y1105@outlook.jp>
2024-12-04 17:52:39 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = "****";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -11,7 +12,7 @@ router.get('/', async (req, res) => {
   const database = client.db('notes');
   const notes = database.collection('notes');
   
-  // idが1のドキュメントを取得
+  // idが2のドキュメントを取得
   const query = { id: 2 };
   const note = await notes.findOne(query);
   
